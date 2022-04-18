aa92395efbb7e0c49a02993092deafa30731b978
Duongntd <103487096+Duongntd@users.noreply.github.com>
2022-04-18 22:35:50 +0200

    added

---
+++ script.js
@@ -0,0 +1,596 @@
+var dots = [
+  {x: 0,
+   y: 0,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 1,
+   y: 0,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 2,
+   y: 0,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 3,
+   y: 0,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 4,
+   y: 0,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 5,
+   y: 0,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 6,
+   y: 0,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 7,
+   y: 0,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 8,
+   y: 0,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 9,
+   y: 0,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 0,
+   y: 1,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 1,
+   y: 1,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 2,
+   y: 1,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 3,
+   y: 1,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 4,
+   y: 1,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 5,
+   y: 1,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 6,
+   y: 1,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 7,
+   y: 1,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 8,
+   y: 1,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 9,
+   y: 1,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 0,
+   y: 2,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 1,
+   y: 2,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 2,
+   y: 2,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 3,
+   y: 2,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 4,
+   y: 2,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 5,
+   y: 2,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 6,
+   y: 2,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 7,
+   y: 2,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 8,
+   y: 2,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 9,
+   y: 2,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 0,
+   y: 3,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 1,
+   y: 3,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 2,
+   y: 3,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 3,
+   y: 3,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 4,
+   y: 3,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 5,
+   y: 3,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 6,
+   y: 3,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 7,
+   y: 3,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 8,
+   y: 3,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 9,
+   y: 3,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 0,
+   y: 4,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 1,
+   y: 4,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 2,
+   y: 4,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 3,
+   y: 4,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 4,
+   y: 4,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 5,
+   y: 4,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 6,
+   y: 4,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 7,
+   y: 4,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 8,
+   y: 4,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 9,
+   y: 4,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 0,
+   y: 5,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 1,
+   y: 5,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 2,
+   y: 5,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 3,
+   y: 5,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 4,
+   y: 5,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 5,
+   y: 5,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 6,
+   y: 5,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 7,
+   y: 5,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 8,
+   y: 5,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 9,
+   y: 5,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 0,
+   y: 6,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 1,
+   y: 6,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 2,
+   y: 6,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 3,
+   y: 6,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 4,
+   y: 6,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 5,
+   y: 6,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 6,
+   y: 6,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 7,
+   y: 6,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 8,
+   y: 6,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 9,
+   y: 6,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 0,
+   y: 7,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 1,
+   y: 7,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 2,
+   y: 7,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 3,
+   y: 7,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 4,
+   y: 7,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 5,
+   y: 7,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 6,
+   y: 7,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 7,
+   y: 7,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 8,
+   y: 7,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 9,
+   y: 7,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 0,
+   y: 8,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 1,
+   y: 8,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 2,
+   y: 8,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 3,
+   y: 8,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 4,
+   y: 8,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 5,
+   y: 8,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 6,
+   y: 8,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 7,
+   y: 8,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 8,
+   y: 8,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 9,
+   y: 8,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 0,
+   y: 9,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 1,
+   y: 9,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 2,
+   y: 9,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 3,
+   y: 9,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 4,
+   y: 9,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 5,
+   y: 9,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 6,
+   y: 9,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 7,
+   y: 9,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 8,
+   y: 9,
+   state: 0,
+   stateChange: 0
+  },
+  {x: 9,
+   y: 9,
+   state: 0,
+   stateChange: 0
+  }
+];
+
+
+function gameStart() {
+ for (let i = 0; i < dots.length; i++) {
+  var stateAround = dots.map(function(cord) {
+      if (cord.x <= dots[i].x + 1 && cord.x >= dots[i].x - 1
+        &&cord.y <= dots[i].y + 1 && cord.y >= dots[i].y - 1) {
+        return cord.state;
+      } else {
+        return 0;
+      }
+    }).reduce((a,b) => a += b)
+  if (dots[i].state === 1) {
+    if (stateAround <= 2) {
+      dots[i].stateChange = -1;
+    } else if (stateAround >= 5) {
+      dots[i].stateChange = -1;
+    }
+  } else if (dots[i].state === 0) {
+     if (stateAround === 3) {
+      dots[i].stateChange = 1;
+     } else {
+       dots[i].stateChange = 0;
+     }
+  }
+ }
+ for (let i = 0; i < dots.length; i++) {
+   if (dots[i].stateChange === 1) {
+    dots[i].state = 1;
+    dots[i].stateChange = 0;
+   } else if (dots[i].stateChange === -1) {
+    dots[i].state = 0;
+    dots[i].stateChange = 0;
+   } else {
+    dots[i].state = dots[i].state;
+   }
+  }
+}
+
+
+for (let i = 0; i < 100; i++) {
+  var newDiv = document.createElement('div');
+  var h1 = document.querySelector('.random');
+  newDiv.className = 'white';
+  newDiv.id = i;
+  newDiv.setAttribute('name', 'dots')
+  newDiv.setAttribute('onclick', `changeState(${i})`)
+  document.querySelector('.checkBoard').insertBefore(newDiv, h1)
+}
+  
+var whiteDots = document.getElementsByName('dots');
+
+function bindArr() {
+  for (i = 0; i < whiteDots.length; i++) {
+   for (j = 0; j < dots.length; j++) {
+     if (i === j) {
+        if (whiteDots[i].className === 'green') {
+          dots[j].state = 1;
+        } else {dots[j].state = 0}
+        if (dots[j].state === 1) {
+          whiteDots[i].setAttribute('class', 'green');
+        } else {whiteDots[i].setAttribute('class', 'white')}
+      }
+    }
+  }
+}
+function nextMove() {
+  bindArr();
+  gameStart();
+  for (i = 0; i < whiteDots.length; i++) {
+   for (j = 0; j < dots.length; j++) {
+     if (i === j) {
+        if (dots[j].state === 1) {
+          whiteDots[i].setAttribute('class', 'green');
+        } else {whiteDots[i].setAttribute('class', 'white')}
+      }
+    }
+  }
+}
+
+function changeState(num) {
+  var dotId = document.getElementById(`${num}`);
+  if (dotId.className === 'white') {
+    dotId.className = 'green';
+  } else {
+    dotId.className = 'white';
+  }
+  bindArr();
+}
+
+
+document.getElementById('btn').addEventListener('click', nextMove);
+
+
